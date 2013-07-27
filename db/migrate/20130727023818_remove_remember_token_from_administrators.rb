f66c834af83f3ff28601da63de22e98b4d29f171
Aroon Sharma <aroonsharma1@yahoo.com>
2013-07-27 00:49:55 -0400

    removed a migration file for admins

--- db/migrate/20130727023818_remove_remember_token_from_administrators.rb
+++
@@ -1,9 +0,0 @@
-class RemoveRememberTokenFromAdministrators < ActiveRecord::Migration
-  def up
-    remove_column :administrators, :remember_token
-  end
-
-  def down
-    add_column :administrators, :remember_token, :string
-  end
-end
